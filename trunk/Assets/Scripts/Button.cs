30e6e1b1c147689c553db0146fef7c5d00fbd863
nata.sesteves@gmail.com <nata.sesteves@gmail.com@b90bc550-515c-9c5d-5bcd-f243b81e259d>
2014-10-01 22:32:35 +0000

    Mudanças de cena

---
+++ trunk/Assets/Scripts/Button.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	public string Scene;
+	public Sprite mouseOverSprite;
+	public Sprite mouseExitSprite;
+
+	SpriteRenderer renderer;
+
+	// Use this for initialization
+	void Start () 
+	{
+		renderer = GetComponent<SpriteRenderer>();
+		mouseExitSprite = renderer.sprite;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnMouseOver() 
+	{
+		renderer.sprite = mouseOverSprite;
+	}
+
+	void OnMouseExit()
+	{
+		renderer.sprite = mouseExitSprite;
+	}
+
+	void OnMouseDown()
+	{
+
+		Application.LoadLevel(Scene);
+	}
+
+}
